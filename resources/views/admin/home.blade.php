f5e48ed047d39e68a69ad5b3e7121e11276ab237
Xyber Pastoril <cyboryan@gmail.com>
2021-07-29 18:46:03 +0800

    Temp. add 'as admin' on admin homepage/dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in as admin!') }}
                 </div>
             </div>
         </div>
