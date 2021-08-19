67d0fae7136b22a165805e921e08b3f456dd0d9b
Justin Manigo <Justin_Manigo@epam.com>
2021-08-20 00:22:04 +0800

    fix ui login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 form">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
@@ -39,17 +39,6 @@
                             </div>
                         </div>
 
-                        <!-- <div class="form-group row">
-                            <div class="col-lg-6 offset-lg-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div> -->
 
                         <div class="form-group row mb-0">
                             <div class="col-lg-8 offset-lg-4">
